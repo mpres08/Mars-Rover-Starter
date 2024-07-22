906fd58cad6fb13184d2d9b4e9357c18ccc19a8c
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-07-21 21:28:03 -0500

    upload part command tests

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,8 +5,18 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+  // TEST 1 
+  test("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+  // TEST 2
+  test("constructor sets command type", function() {
+    expect(this.commandType).toEqual(undefined);
+  });
+
+  // TEST 3
+  test("constructor sets a value passed in as the 2nd argument", function(){
+    expect(this.value).toEqual(undefined);
+  });
 });
