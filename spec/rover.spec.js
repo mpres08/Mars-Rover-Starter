a89ceb834d240ef18a62bbbe0900557c1646224b
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-07-24 20:54:40 -0500

    passing test 7

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,35 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  // TEST 7
+  test("constructor sets position and default values for mode and generatorWatts", function() {
+    const position = "position";
+    const mode = "NORMAL";
+    const generatorWatts = 110;
+    const rover = new Rover (position, mode, generatorWatts);
+    expect(rover.position).toEqual("position")
+    expect(rover.mode).toEqual("NORMAL");
+    expect(rover.generatorWatts).toEqual(110);
+  });
+
+  // TEST 8
+  test("response returned by receiveMessage contains the name of the message", function() {
+    expect(Rover.receiveMessage())
+  });
+
+  // TEST 9
+
+
+  // TEST 10
+
+
+  // TEST 11
+
+
+  // TEST 12
+
+
+  // TEST 13
+
 
 });
