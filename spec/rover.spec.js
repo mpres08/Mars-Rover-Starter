adb921d478cf4fb3de9790cff7d0393ddd4c6ba1
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-07-31 22:23:04 -0500

    upload all passing tests

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -12,16 +12,16 @@ describe("Rover class", function() {
   test("constructor sets position and default values for mode and generatorWatts", function() {
     const rover = new Rover(2000);
 
-    expect(rover.position).toEqual(2000)
+    expect(rover.position).toEqual(2000);
     expect(rover.mode).toEqual("NORMAL");
     expect(rover.generatorWatts).toEqual(110);
   });
 
   // TEST 8
   test("response returned by receiveMessage contains the name of the message", function() {
-    const rover = new Rover(9832);
     const commands = [new Command('STATUS_CHECK'), new Command('MODE_CHANGE', 'LOW_POWER')];
     const message = new Message("Test", commands);
+    const rover = new Rover(2000);
     const response = rover.receiveMessage(message).message;
 
     expect(response).toEqual(message.name);
@@ -32,7 +32,7 @@ describe("Rover class", function() {
     const rover = new Rover(9832);
     const commands = [new Command('STATUS_CHECK'), new Command('MODE_CHANGE', 'LOW_POWER')];
     const message = new Message("Test", commands);
-    const response = rover.receiveMessage(message).response.results.length;
+    const response = rover.receiveMessage(message).results.length;
 
     expect(response).toEqual(2);
   });
@@ -42,53 +42,49 @@ describe("Rover class", function() {
     const rover = new Rover(9832);
     const commands = [new Command('STATUS_CHECK')];
     const message = new Message("Test", commands);
-    const response = rover.receiveMessage(message).response.results;
-
-    expect(response.length).toEqual(1);
-    expect(response[0].completed).toEqual(true);
-    expect(response[0].roverStatus).toEqual({
-      mode: "NORMAL",
-      generatorWatts: 110,
-      position: 9832
-    });
+    const response = rover.receiveMessage(message).results[0];
+    let expected = {
+      completed: true,
+               roverStatus: {
+                  mode: "NORMAL",
+                  generatorWatts: 110,
+                  position: 9832
+               }
+    }
+    expect(response).toEqual(expected);
   });
 
   // TEST 11
   test("responds correctly to the mode change command", function() {
-    const rover = new Rover(9832);
-    const commands = [{commandType: "MODE_CHANGE", newMode: "LOW_POWER"}];
-    const message = new Message("Test", commands);
-    const response = rover.receiveMessage(message).response.results;
+    const commands = [new Command("MODE_CHANGE", "LOW_POWER")];
+    const message = new Message("test 11 name", commands);
+    const rover = new Rover(2000);
+    const response = rover.receiveMessage(message);
 
-    expect(response.length).toEqual(1);
-    expect(response[0].completed).toEqual(true);
     expect(rover.mode).toEqual("LOW_POWER");
+    expect(response.results[0].completed).toEqual(true);
   });
 
   // TEST 12
   test("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
-    const rover = new Rover(9832);
-    const commands = [
-      {commandType: "MODE_CHANGE", newMode: "LOW_POWER"}, 
-      {commandType: "MOVE", newPosition: 9833}
-    ];
+    const rover = new Rover(20);
+    const commands = [new Command("MODE_CHANGE", "LOW_POWER"), new Command("MOVE", 10)];
     const message = new Message("Test", commands);
-    const response = rover.receiveMessage(message).response.results;
+    const response = rover.receiveMessage(message);
 
     expect(rover.mode).toEqual("LOW_POWER");
-    expect(response[1].completed).toEqual(false);
-    expect(rover.position).toEqual(9832);
+    expect(response.results[1].completed).toEqual(false);
+    expect(rover.position).toEqual(20);
+    expect(response.results[0].completed).toEqual(true);
   });
 
   // TEST 13
   test("responds with the position for the move command", function() {
     const rover = new Rover(9832);
-    const commands = [{commandType: "MOVE", newPosition: 9833}];
+    const commands = [new Command("MOVE", 9833)];
     const message = new Message("Test", commands);
-    const response = rover.receiveMessage(message).response.results;
+    const response = rover.receiveMessage(message);
 
-    expect(response.length).toEqual(1);
-    expect(response[0].completed).toEqual(true);
     expect(rover.position).toEqual(9833);
   });
 });
